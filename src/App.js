b6ce6192089fe37a0458d410854505759ab60ab2
Anas-Aqeel <anasaqeel.5555@gmail.com>
2022-08-25 18:19:43 -0700

    UI Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react'
-import Product from './pages/Product'
+import React from "react";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Checkout from "./pages/Checkout";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Product from "./pages/Product";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Product />
-  )
-}
+    <>
 
-export default App
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/shop" element={<Products />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/product/:id" element={<Product />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
